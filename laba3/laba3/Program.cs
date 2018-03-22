ff958fbcbf18504511a242571ce5008fcf525850
Saeid <2011_sa@mail.ru>
2018-03-22 14:23:49 +0700

    first commit

---
+++ laba3/laba3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("menu");
+            Console.WriteLine("nazhmite 1 i vvedite a");
+            Console.WriteLine("nazhmite 2 i vvedite b");
+            Console.WriteLine("vvedite simvol: \n3: +\n4: -\n5: *\n6: /");
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            char ch = key.KeyChar;
+        }
+    }
+}
